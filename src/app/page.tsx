03809d803e77f5844e08dd6904cc41f461283c93
TarunPatidar04 <tarun.coderfarm@gmail.com>
2025-07-31 01:38:59 +0530

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="bg-black text-white h-screen flex items-center justify-center text-3xl font-bold">
+      ✅ Tailwind working perfectly!
+    </div>
+  );
+}
